94afdceb38b1903d2f34fd9cf6dc9431d7a5107e
VadimKorobec <_s_e_t__@ukr.net>
2024-04-21 13:55:47 +0300

    add redux-persist

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,21 @@
 import { createStore } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
 import { devToolsEnhancer } from "@redux-devtools/extension";
 import { rootReducer } from "./rootReducer";
 
 const enhancer = devToolsEnhancer();
 
-export const store = createStore(rootReducer, enhancer);
+const persistConfig = {
+  key: "root",
+  storage,
+  //   whitelist: [],
+  //   blacklist: [],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = createStore(persistedReducer, enhancer);
+
+export const persistor = persistStore(store);
